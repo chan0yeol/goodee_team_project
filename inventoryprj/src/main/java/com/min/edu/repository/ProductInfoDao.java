18174f43027584145b9c9af5965d72eb9c7cec9a
rlawldnjs512 <rlawldnjs512@gmail.com>
2024-12-19 11:50:53 +0900

    docs : 제품(P100) 쿼리테스트 끝났다~!

--- inventoryprj/src/main/java/com/min/edu/repository/ProductInfoDao.java
+++ inventoryprj/src/main/java/com/min/edu/repository/ProductInfoDao.java
@@ -1,8 +1,21 @@
 package com.min.edu.repository;
 
+import java.util.List;
+import java.util.Map;
+
 import com.min.edu.dto.ProductInfoDto;
 
 public interface ProductInfoDao {
 
 	public int product_insert(ProductInfoDto dao);
+	
+	public int product_insert2(ProductInfoDto dao);
+	
+	public List<ProductInfoDto> product_select();
+	
+	public ProductInfoDto product_select2(String string);
+	
+	public List<ProductInfoDto> product_select3(String string);
+	
+	public List<ProductInfoDto> product_select4();
 }
--- inventoryprj/src/main/java/com/min/edu/repository/ProductInfoDaoImpl.java
+++ inventoryprj/src/main/java/com/min/edu/repository/ProductInfoDaoImpl.java
@@ -1,5 +1,8 @@
 package com.min.edu.repository;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.log4j.Logger;
@@ -20,5 +23,40 @@ public class ProductInfoDaoImpl implements ProductInfoDao {
 		session.commit();
 		return cnt;
 	}
+	
+	@Override
+	public int product_insert2(ProductInfoDto dao) {
+		SqlSession session = manager.openSession();
+		int cnt = session.insert(NS+"product_insert2",dao);
+		session.commit();
+		return cnt;
+	}
+	
+	@Override
+	public List<ProductInfoDto> product_select() {
+		SqlSession session = manager.openSession();
+		return session.selectList(NS+"product_select");
+	}
+	
+	@Override
+	public ProductInfoDto product_select2(String string) {
+		ProductInfoDto dto = null;
+		SqlSession session = manager.openSession();
+		dto = session.selectOne(NS+"product_select2",string);
+		return dto;
+	}
+	
+	@Override
+	public List<ProductInfoDto> product_select3(String string) {
+		SqlSession session = manager.openSession();
+		return session.selectList(NS+"product_select3",string);
+	}
+	
+	@Override
+	public List<ProductInfoDto> product_select4() {
+		SqlSession session = manager.openSession();
+		return session.selectList(NS+"product_select4");
+	}
+	
 
 }
