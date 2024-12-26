46594a457bc80f507f62a4a692686f3249450f7e
rlawldnjs512 <rlawldnjs512@gmail.com>
2024-12-26 14:06:17 +0900

    @ chore : servlet class

---
+++ inventoryprj/src/main/java/com/min/edu/ctrl/productInsertServlet.java
@@ -0,0 +1,23 @@
+package com.min.edu.ctrl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class productInsertServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -7303824575715046299L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+}
